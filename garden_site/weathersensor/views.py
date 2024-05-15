001a13095f6cf638ab790136723af2d44d00a241
cgrahlmann <othnin@gmail.com>
2024-05-15 17:10:57 -0500

    First attempt at fixing excessive weather stores

--- garden_site/weathersensor/views.py
+++ garden_site/weathersensor/views.py
@@ -10,12 +10,6 @@ from .forms import CityForm
 def home(request):
     print("weather sensor view home")
 
-    city_deleted = request.GET.get('deleted', False)
-    print(city_deleted)
-
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+os.getenv("OPENWEATHER_KEY")
-    cities = City.objects.all() #return all the cities in the database
-
     if request.method == 'POST':
         form = CityForm(request.POST)
         if form.is_valid():
@@ -26,35 +20,57 @@ def home(request):
     else:
         form = CityForm()
 
-    weather_data = []
-    for city in cities:
-        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+    print(request)
+    city_deleted = request.GET.get('deleted', False)  #were we redirected from the delete page
+    print(city_deleted)
+    if not city_deleted:  #If not get current weather data and save it.
+        print("Save Data to DB ")
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+os.getenv("OPENWEATHER_KEY")
+        cities = City.objects.all() #return all the cities in the database
 
-        weather = {
-            'city' : city,
-            'temperature' : city_weather['main']['temp'],
-            'description' : city_weather['weather'][0]['description'],
-            'icon' : city_weather['weather'][0]['icon']
-        }
+        weather_data = []
+        for city in cities:
+            city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
 
-        weather_data.append(weather) #add the data for the current city into our list for display
-        w = Weather(city=city, main= city_weather['weather'][0]['main'], temp=city_weather['main']['temp'],
-                            description=city_weather['weather'][0]['description'], feels_like=city_weather['main']['feels_like'],
-                            temp_min=city_weather['main']['temp_min'], temp_max=city_weather['main']['temp_max'],
-                            pressure=city_weather['main']['pressure'], humidity=city_weather['main']['humidity'],
-                            visibility=city_weather['visibility'], wind_speed=city_weather['wind']['speed'],
-                            wind_deg=city_weather['wind']['deg'], sunrise = city_weather['sys']['sunrise'], sunset = city_weather['sys']['sunset']
-                            )
-        w.save()
+            weather = {
+                'city' : city,
+                'temperature' : city_weather['main']['temp'],
+                'description' : city_weather['weather'][0]['description'],
+                'icon' : city_weather['weather'][0]['icon']
+            }
+
+            weather_data.append(weather) #add the data for the current city into our list for display
+            w = Weather(city=city, main= city_weather['weather'][0]['main'], temp=city_weather['main']['temp'],
+                                description=city_weather['weather'][0]['description'], feels_like=city_weather['main']['feels_like'],
+                                temp_min=city_weather['main']['temp_min'], temp_max=city_weather['main']['temp_max'],
+                                pressure=city_weather['main']['pressure'], humidity=city_weather['main']['humidity'],
+                                visibility=city_weather['visibility'], wind_speed=city_weather['wind']['speed'],
+                                wind_deg=city_weather['wind']['deg'], sunrise = city_weather['sys']['sunrise'], sunset = city_weather['sys']['sunset']
+                                )
+            w.save()
+
+    else:   #If so then we will get the saved data
+       print("Retrieve data from DB")
+       cities = City.objects.all() 
+       weather_data = []
+       for city in cities:
+            print(city.name)
+            weather = {
+                'city': city.name,
+                'temperature' : city.weather_set.last().temp,
+                'description' : city.weather_set.last().description,
+            }
+            weather_data.append(weather)
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weathersensor_home.html', context)          
 
     
 
-    context = {'weather_data' : weather_data, 'form' : form}
-    return render(request, 'weathersensor_home.html', context)
+
 
 
 def deleteCity(request, id=None):
-    print('deletecity')
+    print('deletecity view')
     #city = get_object_or_404(City, name=id)
     try:
         city = City.objects.get(name=id)
