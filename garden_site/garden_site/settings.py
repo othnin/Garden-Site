f041bbe1ab56c7e451e00391851f6c2cebf3f532
cgrahlmann <othnin@gmail.com>
2024-05-22 09:55:03 -0500

    Added dashboard. But width is not correct anymore

--- garden_site/garden_site/settings.py
+++ garden_site/garden_site/settings.py
@@ -140,7 +140,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
 # Add this below line in settings.py
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- garden_site/garden_site/views.py
+++ garden_site/garden_site/views.py
@@ -1,7 +1,35 @@
 
+import requests, os
 from django.shortcuts import render
-
+from weathersensor.models import City, Weather
 
 def home(request):
     print("garden_site home view")
-    return render(request, 'home.html', {})
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+os.getenv("OPENWEATHER_KEY")
+    cities = City.objects.all() #return all the cities in the database
+
+    weather_data = []
+    for city in cities:
+        print(city)
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list for display
+        w = Weather(city=city, main= city_weather['weather'][0]['main'], temp=city_weather['main']['temp'],
+                            description=city_weather['weather'][0]['description'], feels_like=city_weather['main']['feels_like'],
+                            temp_min=city_weather['main']['temp_min'], temp_max=city_weather['main']['temp_max'],
+                            pressure=city_weather['main']['pressure'], humidity=city_weather['main']['humidity'],
+                            visibility=city_weather['visibility'], wind_speed=city_weather['wind']['speed'],
+                            wind_deg=city_weather['wind']['deg'], sunrise = city_weather['sys']['sunrise'], sunset = city_weather['sys']['sunset']
+                            )
+        w.save()
+
+    context = {'weather_data' : weather_data}
+
+    return render(request, 'home.html', context)
