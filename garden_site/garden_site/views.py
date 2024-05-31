bcb896fb9fc40a024043d8b89b47dc789dd583ae
cgrahlmann <othnin@gmail.com>
2024-05-31 14:07:48 -0500

    Fix bug for no cities in DB views.

--- garden_site/garden_site/views.py
+++ garden_site/garden_site/views.py
@@ -8,23 +8,24 @@ def home(request):
     cities = City.objects.all() #return all the cities in the database
 
     weather_data = []
-    for city in cities:
-        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
-        weather = {
-            'city' : city,
-            'temperature' : city_weather['main']['temp'],
-            'description' : city_weather['weather'][0]['description'],
-            'icon' : city_weather['weather'][0]['icon']
-        }
-        weather_data.append(weather) #add the data for the current city into our list for display
-        w = Weather(city=city, main= city_weather['weather'][0]['main'], temp=city_weather['main']['temp'],
-                            description=city_weather['weather'][0]['description'], feels_like=city_weather['main']['feels_like'],
-                            temp_min=city_weather['main']['temp_min'], temp_max=city_weather['main']['temp_max'],
-                            pressure=city_weather['main']['pressure'], humidity=city_weather['main']['humidity'],
-                            visibility=city_weather['visibility'], wind_speed=city_weather['wind']['speed'],
-                            wind_deg=city_weather['wind']['deg'], sunrise = city_weather['sys']['sunrise'], sunset = city_weather['sys']['sunset']
-                    )
-        w.save()
+    if cities.exists():
+        for city in cities:
+            city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+            weather = {
+                'city' : city,
+                'temperature' : city_weather['main']['temp'],
+                'description' : city_weather['weather'][0]['description'],
+                'icon' : city_weather['weather'][0]['icon']
+            }
+            weather_data.append(weather) #add the data for the current city into our list for display
+            w = Weather(city=city, main= city_weather['weather'][0]['main'], temp=city_weather['main']['temp'],
+                                description=city_weather['weather'][0]['description'], feels_like=city_weather['main']['feels_like'],
+                                temp_min=city_weather['main']['temp_min'], temp_max=city_weather['main']['temp_max'],
+                                pressure=city_weather['main']['pressure'], humidity=city_weather['main']['humidity'],
+                                visibility=city_weather['visibility'], wind_speed=city_weather['wind']['speed'],
+                                wind_deg=city_weather['wind']['deg'], sunrise = city_weather['sys']['sunrise'], sunset = city_weather['sys']['sunset']
+                        )
+            w.save()
 
     context = {'weather_data' : weather_data}
 
